59134f0c028fdf860532f2aed58fb7e6d0a8679c
Abbas Hosseini <hosseini44444@gmail.com>
2021-12-31 10:58:40 +0530

    2022 update

---
+++ _next/static/SiqVbpDwDgtRTbehm2JAV/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/854-0caa63e7ea60e78373dd.js","static/chunks/pages/index-315208c85e4c41922fa0.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
--- _next/static/lipfmJRZdCJLf3wDZ-jmL/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/854-18502d60bb3f5153f746.js","static/chunks/pages/index-2ea12b2a729d9885c7a4.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
